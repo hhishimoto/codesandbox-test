1d324debeb81ea244cf21f713bc659755868808b
hhishimoto <88486927+hhishimoto@users.noreply.github.com>
2022-05-07 13:43:06 +0900

    20_アロー関数

--- src/index.js
+++ src/index.js
@@ -75,5 +75,36 @@ const message1 = "私の名前は" + name + "です。年齢は" + age + "です
 console.log(message1);
 
 // テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
+const message2 = `私の名前は${name}でーす。年齢は${age}でーす。`;
 console.log(message2);
+
+/**
+ * アロー関数
+ */
+console.log("---------- アロー関数 ----------");
+
+// 従来の関数
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です"));
+
+// 従来の関数2
+const func2 = function (str) {
+  return str;
+};
+console.log(func2("func2です"));
+
+// アロー関数
+const func3 = (str) => {
+  return str;
+};
+console.log(func3("func3(アロー関数)です"));
+
+// アロー関数　※処理が１行の場合はretrunを省略できる
+const func4 = (str) => str;
+console.log(func4("func4(アロー関数)です"));
+
+// アロー関数で足し算
+const func5 = (num1, num2) => num1 + num2;
+console.log(func5(10, 20));
